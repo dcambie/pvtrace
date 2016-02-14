c5fea59b6e060398f5f8f8c42a42c9d9c338d74d
Dario Cambié <d.cambie@tue.nl>
2016-02-15 00:28:33 +0100

    Continue refactoring: add statistics to modules
    NOT YET TESTED! Trace coding needed first..
    
    It would also be nice to have a class to open db from file and integrate into a utility to regenerate statistics on pasat db file

--- scripts/model_validation/Modules.py
+++ scripts/model_validation/Modules.py
@@ -106,3 +106,148 @@ class Reactor(object):
             return reaction_mixture
         else:
             raise Exception('The photocatalyst (', self.reaction,') is unknown!')
+
+class Statistics(object):
+    """
+    Class for analysis of results, based on the produced database.
+    Can also be applied to old database data
+    """
+
+    def __init__(self, database):
+        self.db = database
+        self.photon_generated = len(self.db.uids_generated_photons())
+        self.photon_killed = len(self.db.killed())
+        self.tot = self.photon_generated-self.photon_killed
+        self.non_radiative =  len(self.db.uids_nonradiative_losses())
+
+        edges = ['left', 'right', 'near', 'far']
+        apertures = ['top', 'bottom']
+
+        for surface in edges:
+            self.lsc_luminescent_out[surface] = len(trace.database.uids_out_bound_on_surface(surface, luminescent=True))
+            self.lsc_non_luminescent_out[surface] = len(trace.database.uids_out_bound_on_surface(surface, luminescent=False))
+        self.lsc_luminescent_out_edges = sum(self.lsc_luminescent_out)
+        self.lsc_non_luminescent_out_edges = sum(self.lsc_non_luminescent_out)
+        self.lsc_out_edges = self.lsc_luminescent_out_tot + self.lsc_non_luminescent_out_tot
+
+        for surface in apertures:
+            self.lsc_luminescent_out[surface] = len(trace.database.uids_out_bound_on_surface(surface, luminescent=True))
+            self.lsc_non_luminescent_out[surface] = len(trace.database.uids_out_bound_on_surface(surface, luminescent=False))
+        self.lsc_luminescent_out = sum(self.lsc_luminescent_out)
+        self.lsc_non_luminescent_out = sum(self.lsc_non_luminescent_out)
+        self.lsc_out = self.lsc_luminescent_out_tot + self.lsc_non_luminescent_out_tot
+
+        self.lsc_luminescent_out_apertures = self.lsc_luminescent_out - self.lsc_luminescent_out_edges
+        self.lsc_non_luminescent_out_apertures =
+        self.lsc_out_apertures = self.lsc_out - self.lsc_out_edges
+
+
+    def percent(self, num_photons):
+        """
+        Return the percentage of num_photons with respect to thrown photons as 2 decimal digit string
+        """
+        return format((num_photons / self.tot)*100,'.2f')
+
+    def print_report(self):
+        print "##### PVTRACE LOG RESULTS #####"
+
+        print "Summary:"
+        print "\t Photon efficiency \t", (luminescent_edges + luminescent_apertures) * 100 / thrown, "%"
+        #print "\t Optical efficiency \t", luminescent_edges * 100 / thrown, "%"
+
+
+
+
+    def print_detailed(self):
+        self.print_report()
+
+        print "Technical details:"
+        print "\t Generated \t", self.photon_generated
+        print "\t Killed \t", self.photon_killed
+        print "\t Thrown \t", self.tot
+
+        print "Luminescent photons:"
+
+        edges = ['left', 'near', 'far', 'right']
+        apertures = ['top', 'bottom']
+
+        for surface in edges:
+            print "\t", surface, "\t", len(
+                self.db.uids_out_bound_on_surface(surface, luminescent=True)) / thrown * 100, "%"
+
+        for surface in apertures:
+            print "\t", surface, "\t", len(
+                self.db.uids_out_bound_on_surface(surface, luminescent=True)) / thrown * 100, "%"
+
+        print "Non radiative losses\t", non_radiative_photons / thrown * 100, "%"
+
+        print "Solar photons (transmitted/reflected):"
+        for surface in edges:
+            print "\t", surface, "\t", len(
+                self.db.uids_out_bound_on_surface(surface, solar=True)) / thrown * 100, "%"
+
+        for surface in apertures:
+            print "\t", surface, "\t", len(
+                self.db.uids_out_bound_on_surface(surface, solar=True)) / thrown * 100, "%"
+
+        print "Reactor's channel photons:"
+
+
+        photons = trace.database.uids_in_reactor()
+        photons_in_channels_tot = len(photons)
+        luminescent_photons_in_channels = len(trace.database.uids_in_reactor_and_luminescent())
+        # oNLY PHOTONS IN CHANNELS!
+        for photon in photons:
+            if config['debug']:
+                print "Wavelenght: ", trace.database.wavelengthForUid(photon)  # Nice output
+            elif config['print_waveleghts']:
+                print " ".join(map(str, trace.database.wavelengthForUid(photon)))  # Clean output (for elaborations)
+
+        if config['debug']:
+            print channel.name, " photons: ", photons_in_channels_tot / thrown * 100, "% (", photons_in_channels_tot, ")"
+
+        if config['informative_output']:
+            print "Photons in channels (sum)", photons_in_channels_tot / thrown * 100, "% (", photons_in_channels_tot, ")"
+
+        # Put header here just if needed (other output on top)
+        if config['print_summary'] == True and config['informative_output'] == True:
+            print header
+        if config['print_summary']:
+            top_reflected = len(trace.database.uids_out_bound_on_surface("top", solar=True))
+            bottom_lost = len(trace.database.uids_out_bound_on_surface("bottom", solar=True))
+            print thrown, "\t", top_reflected, "\t", bottom_lost, "\t", luminescent_edges, "\t", luminescent_apertures, "\t", (
+            photons_in_channels_tot - luminescent_photons_in_channels), "\t", luminescent_photons_in_channels, "\t", non_radiative_photons
+
+        if config['debug']:  # Check coherence of results
+            if top_reflected + bottom_lost + luminescent_edges + luminescent_apertures + photons_in_channels_tot + non_radiative_photons == thrown:
+                print "Results validity check OK :)"
+            else:
+                print "!!! ERROR !!! Check results carefully!"
+    def create_graphs(self):
+        import os
+        import numpy as np
+        import pylab
+
+        home = os.path.expanduser("~")
+
+        print "Plotting reactor..."
+        uid = self.db.uids_in_reactor()
+        #print "Photons in channels array is: ",uid
+        data = self.db.wavelengthForUid(uid)
+        hist = np.histogram(data, bins=np.linspace(300,800,num=100))
+        pylab.hist(data, 100, histtype='stepfilled')
+        location = os.path.join(home,"pvtrace_export","plot-reactor.png")
+        pylab.savefig(location)
+        print 'Plot saved in ', location, '!'
+        pylab.clf()
+
+        print "Plotting reactor luminescent..."
+        uid = db.uids_in_reactor_and_luminescent()
+        #print "Photons in channels array is: ",uid
+        data = db.wavelengthForUid(uid)
+        hist = np.histogram(data, bins=np.linspace(300,800,num=100))
+        pylab.hist(data, 100, histtype='stepfilled')
+        location = os.path.join(home,"pvtrace_export","plot-reactor-luminescent.png")
+        pylab.savefig(location)
+        print 'Plot saved in ', location, '!'
+        pylab.clf()
--- scripts/model_validation/simulation.py
+++ scripts/model_validation/simulation.py
@@ -14,12 +14,29 @@ reactor = Reactor(name="5x5_8ch", dye="Red305", dye_concentration=0.15, photocat
 for obj in reactor.scene_obj:
     scene.add_object(obj)
 
-# trace = Tracer(scene=scene, source=source, seed=None, database_file=dbfile, throws=250, use_visualiser=True, show_log=False)
-# trace.show_lines = True
-# trace.show_path = True
-# import time
-# tic = time.clock()
-# trace.start()
-# toc = time.clock()
+# Ask python that the directory of this script file is and use it as the location of the database file
+pwd = os.getcwd()
+dbfile = os.path.join(pwd, 'pvtracedb.sql')  # <--- the name of the database file, with "pvtracedb" overwrite is implied
+
+trace = Tracer(scene=scene, source=source, seed=None, throws=10, database_file=dbfile, use_visualiser=false, show_log=false, show_axis=True)
+trace.show_lines = true
+trace.show_path = true
+
+# Run simulation
+tic = time.clock()
+trace.start()
+toc = time.clock()
+
+# For reproducibility reasons always append git version to results (tested on linux only)
+import subprocess
+label = subprocess.check_output(["git", "describe"], cwd=PVTDATA)
+print 'PvTrace ', label,  ' simulation ended'
+print 'Date/Time ', time.strftime("%c")
+print 'Run Time: ', toc - tic, ' sec.(s)'
+print ''
+
+
+stats = Statistics(trace.database)
+stats.print_report()
 
 sys.exit(0)

a5d8e563cd438b26585c317964af5ceb8bd446db
Dario Cambié <d.cambie@tue.nl>
2016-04-03 21:46:18 +0200

    Simulation tim uplevel (from debug to info)

--- scripts/model_validation/simulation.py
+++ scripts/model_validation/simulation.py
@@ -7,8 +7,8 @@ from modules import *
 import sys
 
 # 1 unit = 1 m  Albeit not convenient, this assumption is deeply bounded in pvtrace's heart
-scene = pvtrace.Scene()
-# scene = pvtrace.Scene('overwrite_me')
+# scene = pvtrace.Scene()
+scene = pvtrace.Scene('overwrite_me')
 
 logger = logging.getLogger('pvtrace')
 
@@ -18,8 +18,8 @@ logger = logging.getLogger('pvtrace')
 
 # reactor = Reactor(reactor_name="5x5_6ch_squared", dye="Red305", dye_concentration=0.20, photocatalyst="MB",
 #                   photocatalyst_concentration=0.0004)
-reactor = Reactor(reactor_name="5x5_1ch", dye="Red305", dye_concentration=0.15, photocatalyst="MB",
-                  photocatalyst_concentration=0.01)
+reactor = Reactor(reactor_name="5x5_1ch", dye="Red305", dye_concentration=0.5, photocatalyst="MB",
+                  photocatalyst_concentration=0.00001)
 logger.info('Reactor volume (calculated): '+str(reactor.reaction_volume*1000000)+' mL')
 
 # reactor = Reactor(name="5x5_0ch", dye="Red305", dye_concentration=0.20)
@@ -29,17 +29,17 @@ for obj in reactor.scene_obj:
 # Doesn't save DB file but uses RAM disk for faster simulation
 # file = os.path.join(os.path.expanduser("~"),"pvtracedb.sql")
 # file = None
-trace = pvtrace.Tracer(scene=scene, source=reactor.source, seed=None, throws=500000, use_visualiser=False,
+trace = pvtrace.Tracer(scene=scene, source=reactor.source, seed=None, throws=100, use_visualiser=False,
                        show_log=False, show_axis=True, show_counter=False, db_split=True)
 trace.show_lines = True
 trace.show_path = False
 
 # Run simulation
 tic = time.clock()
-logger.debug('Simulation Started (time: '+str(tic)+')')
+logger.info('Simulation Started (time: '+str(tic)+')')
 trace.start()
 toc = time.clock()
-logger.debug('Simulation Ended (time: '+str(toc)+', elapsed: '+str(toc-tic)+' s)')
+logger.info('Simulation Ended (time: '+str(toc)+', elapsed: '+str(toc-tic)+' s)')
 
 for obj in scene.objects:
     if type(obj) is pvtrace.Devices.LSC:

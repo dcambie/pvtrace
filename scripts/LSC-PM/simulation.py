6fb3fa7d7f4c10867cf83ee4a7a65f117926d2a2
Dario Cambié <d.cambie@tue.nl>
2016-11-21 12:44:28 +0100

    Clean LSC-PM/simulation.py (fully working now)

--- scripts/LSC-PM/simulation.py
+++ scripts/LSC-PM/simulation.py
@@ -1,7 +1,6 @@
 from pvtrace import *
 
 study = Scene(uuid="86jfhfQogE9gE9J6e8MupF", force=True)
-print("as")
-study.log.warn("provaaa")
-# study.stats.print_detailed()
+study.stats.print_detailed()
+
 sys.exit(0)

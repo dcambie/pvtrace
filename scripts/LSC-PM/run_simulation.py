57a2d7e1081f84559503c7341bd5f0c5cc124aaa
Dario Cambié <dario.cambie@gmail.com>
2017-02-11 01:44:24 +0100

    Adds sqlitebck dependences to simplify module compilation

--- scripts/LSC-PM/run_simulation.py
+++ scripts/LSC-PM/run_simulation.py
@@ -1,9 +1,9 @@
 from pvtrace.lscpm.Reactor import *
-from pvtrace.lscpm.dyes import *
+from pvtrace.lscpm.Dyes import *
 from pvtrace.lscpm.Matrix import *
 from pvtrace.lscpm.SolarSimulators import *
 
-scene = pvtrace.Scene()
+scene = pvtrace.Scene(level=logging.INFO)
 logger = logging.getLogger('pvtrace')
 
 # Create LSC-PM DYE material
@@ -16,7 +16,7 @@ reactor = Reactor(reactor_name="5x5_6ch_squared", luminophore=lr305, matrix=pdms
 scene.add_objects(reactor.scene_obj)
 
 lamp = LightSource(lamp_type='SolarSimulator', irradiated_area=(0.05, 0.05), distance=0.025)
-trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=10000, use_visualiser=False,
+trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=1000, use_visualiser=False,
                        show_axis=True, show_counter=False, db_split=True, preserve_db_tables=True)
 trace.show_lines = True
 trace.show_path = False

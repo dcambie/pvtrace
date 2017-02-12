baf819dc48b777619a962e9262aaf0c0f04a81aa
Dario Cambié <dario.cambie@gmail.com>
2017-02-12 21:33:15 +0100

    Modelling Fang reactors

--- data/reactors/writer.py
+++ data/reactors/writer.py
@@ -2,8 +2,8 @@ import os, ConfigParser
 
 config = ConfigParser.SafeConfigParser()
 config.add_section('Main')
-config.set('Main', 'name', 'Reactor (5x5cm, Spyral, 0.35 mm nozzle)')
-config.set('Main', 'description', 'PDMS-based LSC-PM 5x5cm w/ spyral design')
+config.set('Main', 'name', 'Reactor (Fang, 2 channels, 0.35 mm nozzle)')
+config.set('Main', 'description', 'PDMS-based LSC-PM 5x5cm 2 channels Fang')
 config.add_section('LSC')
 config.set('LSC', 'thickness', '0.003')
 config.set('LSC', 'width', '0.05')
@@ -13,21 +13,14 @@ config.add_section('Channels')
 
 geometry = []
 #        ORIGIN:  X        Y     Z  L:   X      Y   Z
-geometry.append(((4.775, 0.000, 1), (1.05, 10.00, 1)))  # Inlet, bigger for the first 10 mm
-geometry.append(((5.125, 10.000, 1), (0.35, 36.15, 1)))  # 1st channel left
-geometry.append(((5.475, 45.800, 1), (31.25, 0.35, 1)))  # 1st channel bottom
-geometry.append(((36.725, 11.650, 1), (0.35, 34.50, 1)))  # 2nd channel right
-geometry.append(((21.075, 11.650, 1), (15.65, 0.35, 1)))  # 2nd channel top
-geometry.append(((20.725, 11.650, 1), (0.35, 18.90, 1)))  # 3rd channel left
-geometry.append(((21.075, 30.200, 1), (3.75, 0.35, 1)))  # 3nd channel bottom
-geometry.append(((24.825, 19.450, 1), (0.35, 11.10, 1)))  # Middle piece
-geometry.append(((25.175, 19.450, 1), (3.75, 0.35, 1)))  # 3nd channel top
-geometry.append(((28.925, 19.450, 1), (0.35, 18.90, 1)))  # 3nd channel right
-geometry.append(((13.275, 38.000, 1), (15.65, 0.35, 1)))  # 2nd channel bottom
-geometry.append(((12.925, 3.850, 1), (0.35, 34.50, 1)))  # 2nd channel left
-geometry.append(((13.275, 3.850, 1), (31.25, 0.35, 1)))  # 1st channel top
-geometry.append(((44.525, 3.850, 1), (0.35, 36.15, 1)))  # 1st channel right
-geometry.append(((44.175, 40.000, 1), (1.05, 10.00, 1)))  # Outlet, bigger for the first 10 mm
+geometry.append(((4.975, 0.000, 1), (1.05, 10.00, 1)))  # Inlet, bigger for the first 10 mm
+geometry.append(((5.325, 10.00, 1), (0.35, 37.675, 1)))  # 1st channel top
+
+geometry.append(((5.675, 47.325, 1), (38.65, 0.35, 1)))  # connection
+
+
+geometry.append(((44.325, 10.000, 1), (0.35, 37.675, 1)))  # last channel bottom
+geometry.append(((43.975,  0.000, 1), (1.05, 10.00, 1)))  # Outlet, bigger for the first 10 mm
 
 # Transform mm into meters (overly complicated code to multiply all values im geometry per 0.001)
 geometry = [[[coord * 0.001 for coord in tuples] for tuples in channel] for channel in geometry]
@@ -40,5 +33,5 @@ for i in range(0, len(geometry)):
 config.set('Channels', 'channels', str(channel))
 
 # Writing our configuration file to 'example.cfg'
-with open('5x5_spyral_035_3_1.ini', 'wb') as configfile:
+with open('5x5_fang_2ch.ini', 'wb') as configfile:
     config.write(configfile)

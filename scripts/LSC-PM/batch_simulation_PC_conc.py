e8e6b947259c9c2c9b9e5c96b7dc48dda23eaf0f
Dario Cambié <dario.cambie@gmail.com>
2017-02-19 14:29:53 +0100

    generalize batch_simulation_PC_conc

--- scripts/LSC-PM/batch_simulation_PC_conc.py
+++ scripts/LSC-PM/batch_simulation_PC_conc.py
@@ -5,27 +5,32 @@ from pvtrace.lscpm.Matrix import *
 from pvtrace.lscpm.SolarSimulators import *
 from math import pow
 
+photocatalyst = 'MB'
+dye = 'Red305'
+dye_conc = 50
+
 file_path = os.path.join(os.path.expanduser('~'), 'pvtrace_data',
-                         'output_reflection.txt')
+                         'MB_grad_'+str(dye_conc)+'.txt')
 
 for mainloop_i in range(0, 21):
     exp = (-50 + mainloop_i)/10
-    mb_conc = math.pow(10, exp)
+    pc_conc = pow(10, exp)
 
     # This implicitly restart logging on the new location
-    scene = pvtrace.Scene('edge_lr305_100_MB_10_' + str(exp))
+    scene = pvtrace.Scene(dye+'_'+str(dye_conc)+'_'+photocatalyst+'_10_' + str(exp))
 
     logger = logging.getLogger('pvtrace')
-    # LR305 200ppm
-    lr305 = LuminophoreMaterial('Red305', 100)
+
+    luminophore = LuminophoreMaterial(dye, dye_conc)
     pdms = Matrix('pdms')
 
-    reactor = Reactor(reactor_name="5x5_6ch_squared", luminophore=lr305, matrix=pdms, photocatalyst="MB",
-                      photocatalyst_concentration=mb_conc, solvent="acetonitrile")
+    reactor = Reactor(reactor_name="5x5_6ch_squared", luminophore=luminophore, matrix=pdms, photocatalyst=photocatalyst,
+                      photocatalyst_concentration=pc_conc, solvent="acetonitrile")
     scene.add_objects(reactor.scene_obj)
 
-    lamp = LightSource(lamp_type='SolarSimulator', irradiated_area=(0.05, 0.05), distance=0.025)
-    trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=10000, use_visualiser=False,
+    lamp = LightSource(lamp_type='SolarSimulator')
+    lamp.set_lightsource(irradiated_area=(0.05, 0.05), distance=0.025)
+    trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100000, use_visualiser=False,
                            show_axis=True, show_counter=False, db_split=True, preserve_db_tables=True)
 
     # Run simulation
@@ -40,7 +45,7 @@ for mainloop_i in range(0, 21):
         text = str(scene.stats.print_excel_header("Concentration")+"\n")
     else:
         text = ""
-    text += str(scene.stats.print_excel(mb_conc) + "\n")
+    text += str(scene.stats.print_excel(pc_conc) + "\n")
     write_me = open(file_path, 'a')
     write_me.write(text)
     write_me.close()

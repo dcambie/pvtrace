98afecc999949fc0f98a7c00cceb6e6415411e7d
Dario Cambié <d.cambie@tue.nl>
2017-02-20 10:12:52 +0100

    Finally finish solvent class with proper refractive index handling

--- pvtrace/lscpm/Solvents.py
+++ pvtrace/lscpm/Solvents.py
@@ -14,36 +14,38 @@ class Solvent(object):
     def __init__(self, solvent_name="acetonitrile"):
         self.logger = logging.getLogger('pvtrace.solvent')
 
-        self.logger.info('Solvent set to "' + str(solvent_name) + '" BUT class Solvent is not implemented. ACN assumed')
-        self.n = 1.3441
+        # Data at 588nm (D-line sodium) from Burdick & Jackson + Wikipedia for some (ACN, DMSO, AcOEt, CHCl3)
+        solvent_list = {(('acetonitrile', 'ACN', 'CH3CN'), 1.3441),
+                        (('water', 'H2O'), 1.333),
+                        (('Air', ''), 1.0),
+                        (('N,N-DiMethylFormammide', 'dimethylformamide', 'DMF'), 1.4305),
 
-        # FIXME implement this
-        list = ((('acetonitrile', 'ACN', 'CH3CN'), 1.3441),
-                (('water', 'H2O'), 1.333),
-                (('Air', ''), 1.0),
-                (('N,N-DiMethylFormammide', 'dimethylformamide', 'DMF'), 1.4305),
+                        # Alcohols
+                        (('Methanol', 'Methyl alcohol', 'MeOH'), 1.3284),
+                        (('Ethanol', 'Ethyl alcohol', 'EtOH'), 1.3614),
+                        (('Isopropanol', 'Isopropyl alcohol', 'EtOH'), 1.3772),
 
-                # Alcohols
-                (('Methanol', 'Methyl alcohol', 'MeOH'), 1.3284),
-                (('Ethanol', 'Ethyl alcohol', 'EtOH'), 1.3614),
-                (('Isopropanol', 'Isopropyl alcohol', 'EtOH'), 1.3772),
+                        # Ethers
+                        (('Tetrahydrofuran', 'THF'), 1.4072),
+                        (('Ethylether', 'Diethylether', 'Et2O'), 1.3524),
+                        (('1,4-dioxane', 'Dioxane'), 1.4224),
 
-                # Ethers
-                (('Tetrahydrofuran', 'THF'), 1.4072),
-                (('Ethylether', 'Diethylether', 'Et2O'), 1.3524),
-                (('1,4-dioxane', 'Dioxane'), 1.4224),
+                        # Alkanes
+                        (('Hexane', 'Hex'), 1.3749),
+                        (('Heptane', 'Hept'), 1.3876),
 
-                # Alkanes
-                (('Hexane', 'Hex'), 1.3749),
-                (('Heptane', 'Hept'), 1.3876),
+                        # Others
+                        (('Acetone'), 1.3586),
+                        (('EthylAcetate', 'AcOEt'), 1.3724),
+                        (('Dichloromethane', 'Methylene chloride', 'DCM'), 1.4241),
+                        (('Dimethylsulfoxide', 'DMSO'), 1.4793),
+                        (('N-Methylpyrrolidone', 'NMP'), 1.4700),
+                        (('Toluene', 'C6H5CH3'), 1.4969)}
 
-                # Others
-                (('Acetone'), 1.3586),
-                (('EthylAcetate', 'AcOEt'), 1.3724),
-                (('Dichloromethane', 'Methylene chloride', 'DCM'), 1.4241),
-                (('Dimethylsulfoxide', 'DMSO'), 1.4793),
-                (('N-Methylpyrrolidone', 'NMP'), 1.4700),
-                (('Toluene', 'C6H5CH3'), 1.4969))
+        for solvent_tuple in solvent_list:
+            if solvent_name in solvent_tuple[0]:
+                self.n = solvent_tuple[1]
+                self.logger.info('Solvent set to "' + str(solvent_name) + '" having a R.I. of '+str(self.n))
 
     def refractive_index(self):
         return self.n

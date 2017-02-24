3c7345296fe63cd00b625849849401a028af3f49
Dario Cambié <dario.cambie@gmail.com>
2017-02-24 19:23:38 +0100

    Analyze simulation to calculate apptd
    Post-run simulation analysis

--- scripts/LSC-PM/analyse_simulation.py
+++ scripts/LSC-PM/analyse_simulation.py
@@ -1,19 +1,30 @@
 from pvtrace import *
-uuid = '5x5_fang_2ch_whiteLED_8V_1k'
-study = Scene(uuid=uuid, force=True, level=logging.DEBUG)
+import numpy as np
+uuid = '1sqm_1dir_borders_100mm'
 
-# db = pvtrace.PhotonDatabase(dbfile=None)
-# db.add_db_file(filename=os.path.join(study.working_dir, 'db.sqlite'))
-# study.stats.add_db(db)
-# study.stats.print_detailed()
+study = Scene(uuid=uuid, force=True, level=logging.INFO)
+logger = logging.getLogger('pvtrace.postrun_analysis')
 
-a = study.stats.calculate_photon_balance()
-study.stats.save_photon_balance(a, uuid)
-
+# PHOTON BALANCE STUDY
+# a = study.stats.calculate_photon_balance()
+# study.stats.save_photon_balance(a, uuid)
+tic = time.clock()
+study.stats.db_stats()
 # uids = study.stats.db.endpoint_uids()
-# a=study.stats.history_from_uid(uid=(63, 6, 125))
-#
-# trjs=study.stats.describe_trajectory(uid_list=a)
+uids = study.stats.uids['luminescent_edges'] + study.stats.uids['luminescent_channel']
+history = study.stats.history_from_uid(uid=uids)
+
+path_length = []
+trj_list = study.stats.describe_trajectory(uid_list=history)
+for trajectory in trj_list:
+    path_length.append(trajectory.total_pathlength())
+avg = np.mean(path_length)
+logger.info("The average Photon-path Lumi_edge + Lumi_channels is "+str(avg*100)+"cm")
+
+toc = time.clock()
+logger.info('Calculating this took ' + str(toc - tic) + ' seconds!')
+
+
 #
 # dis = []
 # for trj in trjs:

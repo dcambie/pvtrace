d2a2ccfb98e08130fc5ff7bf3068d20791b65145
Dario Cambié <d.cambie@tue.nl>
2018-02-12 15:47:21 +0100

    fixing axis visibility status

--- scripts/LSC-PM/run_simulation.py
+++ scripts/LSC-PM/run_simulation.py
@@ -6,35 +6,38 @@ from pvtrace.lscpm.Matrix import *
 from pvtrace.lscpm.SolarSimulators import *
 # blue red thickness: 3mm green: 4mm
 # blue not yet, green EY, red Methylene Blue,
-# set episilon to prevent the possibility of matching fate and generate
-
+# set epsilon to prevent the possibility of matching fate and generate
 
 # scene = pvtrace.Scene(level=logging.INFO, uuid="Fang_rebuttal2_6")
 scene = pvtrace.Scene(level=logging.INFO, uuid="overwrite_me")
 logger = logging.getLogger('pvtrace')
 
 # Create LSC-PM DYE material
-lr305 = LuminophoreMaterial('Red305', 200)#red
-k160 = LuminophoreMaterial('K160', 1)#green
-blue_evonik = LuminophoreMaterial('Evonik_Blue', 1)#blue
+lr305 = LuminophoreMaterial('Red305', 200)              # Red
+k160 = LuminophoreMaterial('K160', 1)                   # Green
+blue_evonik = LuminophoreMaterial('Evonik_Blue', 1)     # Blue
+
 # Create polymeric matrix
 pdms = Matrix('pdms')
 pmma = Matrix('PMMA')
 
+# Create reactor
 reactor = Reactor(reactor_name="chong_red_10x10x0.3cm", luminophore=lr305, matrix=pmma, photocatalyst="MB",
                   photocatalyst_concentration=0.004, solvent='ACN')
 scene.add_objects(reactor.scene_obj)
 
+# Light source
+# LEDs
 # lamp = LightSource(lamp_type='White LEDs')
 # lamp.set_LED_voltage(voltage=8)
-# lamp.set_lightsource(irradiated_area=(0.05, 0.05), distance=0.025)
-lamp = LightSource(lamp_type='SolarSimulator')
-# fixed by chong in order to run the different reactor without altering the scripts
-lamp.set_lightsource(irradiated_length=reactor.lsc_length, irradiated_width=reactor.lsc_width, distance=0.025)
-# lamp.set_lightsource(irradiated_area=(reactor.lsc.size[0], 0.15035), distance=0.025)
-# lamp.move_lightsource(vector=(0, 0.01735))
 
-trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100, use_visualiser=True,
+# Solar simulator
+lamp = LightSource(lamp_type='SolarSimulator')
+
+# Irradiated area
+lamp.set_lightsource(irradiated_area=(reactor.lsc.size[0], reactor.lsc.size[1]), distance=0.025)
+
+trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=1000, use_visualiser=False,
                        show_axis=False, show_counter=False, db_split=True, preserve_db_tables=True)
 # set color on Trace.py while visualizing
 
@@ -62,7 +65,7 @@ print(scene.stats.print_excel() + "\n")
 photons_in_object = {}
 photonsum = 0
 for obj in scene.objects:
-    if type(obj) is pvtrace.Devices.Channel and len(obj.store)>0:
+    if type(obj) is pvtrace.Devices.Channel and len(obj.store) > 0:
         photon_loss = len(obj.store['loss'])
         photons_in_object[obj.name] = photon_loss
 
@@ -72,7 +75,7 @@ print("Channel No, Photons")
 for entry, value in photons_in_object.items():
     print(str(entry)[7:]+", "+str(value))
 
-scene.stats.create_graphs()
+# scene.stats.create_graphs()
 
 toc2 = time.clock()
 t_span = toc2-tic

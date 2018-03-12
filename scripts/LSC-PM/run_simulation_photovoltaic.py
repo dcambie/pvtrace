177e962bbd2140c3ade82870e08727dfeff6ca04
shenchong <shenchong@tju.edu.cn>
2018-03-12 17:51:56 +0100

    the script for simulating the photovoltaic

--- scripts/LSC-PM/run_simulation_photovoltaic.py
+++ scripts/LSC-PM/run_simulation_photovoltaic.py
@@ -7,35 +7,34 @@ from pvtrace.lscpm.SolarSimulators import *
 # blue red thickness: 3mm green: 4mm
 # blue not yet, green EY, red Methylene Blue,
 # set episilon to prevent the possibility of matching fate and generate
-file_path = os.path.join('C:/','LSC_PM_simulation_results', 'photovoltaic', 'PV_redLSC_PM', 'simulation_results.txt')
+file_path = os.path.join('D:/','LSC_PM_simulation_results', 'photovoltaic', 'PV_redLSC_PM', 'simulation_results.txt')
 
 # scene = pvtrace.Scene(level=logging.INFO, uuid="Fang_rebuttal2_6")
-scene = pvtrace.Scene(level=logging.INFO, uuid='overwrite_me')
+scene = pvtrace.Scene(level=logging.INFO, uuid='0312final_try')
 logger = logging.getLogger('pvtrace')
 
-# Create LSC-PM DYE material
-Ev_lr305 = LuminophoreMaterial('Evonik_lr305', 1)#red
-k160 = LuminophoreMaterial('K160', 1)#green
-blue_evonik = LuminophoreMaterial('Evonik_Blue', 1)#blue
-# Create polymeric matrix
-pdms = Matrix('pdms')
+# Create LSC-PM DYE material and host material
+Lm_lr305 = LuminophoreMaterial('Limacryl_lr305', 1)#red
 pmma = Matrix('PMMA')
 
 # !!Change the LSC material while simulating the blank LSC
-reactor = Reactor(reactor_name="10x10_chong_thickness0.3cm", luminophore=Ev_lr305, matrix=pmma, photocatalyst="MB",
-                  photocatalyst_concentration=0.0012, solvent='ACN', exist_photovoltaic=True, blank=False)
+reactor = Reactor(reactor_name="10x10_chong_thickness0.3cm", luminophore=Lm_lr305, matrix=pmma, photocatalyst="MB",
+                  photocatalyst_concentration=0.0012, solvent='ACN', exist_photovoltaic_bottom=True, exist_photovoltaic_edge=True)
 scene.add_objects(reactor.scene_obj)
+# seeeqe = reactor.photovoltaic.external_QE(file_name = 'D:\PvTrace_git\pvtrace-fork\data\photovoltaic\External_quantum_efficiency.txt')
+# print(seeeqe)
 
 # lamp = LightSource(lamp_type='White LEDs')
 # lamp.set_LED_voltage(voltage=8)
 # lamp.set_lightsource(irradiated_area=(0.05, 0.05), distance=0.025)
 lamp = LightSource(lamp_type='Sun')
 
-lamp.set_lightsource(irradiated_length=reactor.lsc.size[0], irradiated_width=reactor.lsc.size[1], distance=0.025)
+lamp.set_lightsource(irradiated_length=reactor.lsc.size[0], irradiated_width=reactor.lsc.size[1], lamp_wavelength = (300, 1100),
+                     distance=0.025)
 # lamp.set_lightsource(irradiated_area=(reactor.lsc.size[0], 0.15035), distance=0.025)
 # lamp.move_lightsource(vector=(0, 0.01735))
 
-trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100, use_visualiser=True,
+trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100, use_visualiser=False,
                        show_axis=False, show_counter=False, db_split=True, preserve_db_tables=True)
 # set color on Trace.py while visualizing
 
@@ -44,8 +43,10 @@ trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100, use_visualis
 trace.start()
 
 scene.stats.print_detailed()
-text = str(scene.stats.print_excel(backscatter=False) + "\n")
+text_head = str(scene.stats.print_excel_header(photovoltaic=True) + "\n")
+text = str(scene.stats.print_excel(photovoltaic=True) + "\n")
 write_me = open(file_path, 'a')
+write_me.write(text_head)
 write_me.write(text)
 write_me.close()
 

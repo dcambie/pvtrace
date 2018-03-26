cb6558e148a4586eac6743ef4c328f3190c2198c
shenchong <shenchong@tju.edu.cn>
2018-03-26 13:20:55 +0200

    add the SMARTS software developed by NREL(an American lab)

--- scripts/LSC-PM/analyse_simulation.py
+++ scripts/LSC-PM/analyse_simulation.py
@@ -1,9 +1,8 @@
 from __future__ import division
 from pvtrace import *
-textws = ''
 
 import numpy as np
-file_path = os.path.join('C:/','LSC_PM_simulation_results', 'Red_PMMA_10x10x0.3_1M', 'tubing_analysis.txt')
+file_path = os.path.join('D:/','LSC_PM_simulation_results', 'Red_PMMA_10x10x0.3_1M', 'tubing_analysis.txt')
 study = Scene(uuid= 'Red_PMMA_10x10x0.3_1M', force=True, level=logging.INFO)
 logger = logging.getLogger('pvtrace.postrun_analysis')
 
@@ -13,31 +12,43 @@ logger = logging.getLogger('pvtrace.postrun_analysis')
 # tic = time.clock()
 study.stats.db_stats()
 # uids = study.stats.db.endpoint_uids()
-uids = study.stats.uids['tot']
-text = study.stats.db.wavelength_for_uid(uids)
+uids_tot = study.stats.uids['tot']
+uids_ref = study.stats.uids['']
+text = study.stats.db.original_wavelength_for_uid(uids)
 
 # To speed up calculation sample a subset of 1k photons
 # uids = np.sort(uids)
 # uids = list(uids[0:10])
-numuid_c0 = len(text)
+numuid_c0 = len(uids)
 
 z = [0]*350
 print(numuid_c0)
 
 write_me = open(file_path, 'a')
+Energy_photon = 0
+h_plank = 6.626 # 10(-34)
+c_photon = 2.998 # 10(8)
 for i in text:
-    for g in range(1,350):
-        g_wave = g*1 + 350
-        if i >= g_wave - 1 and i <= g_wave:
-            z[g] += 1
-
-for i in z:
-    write_me.write(str(i) + '\n')
-# write_me.write(text)
-write_me.close()
-
-sys.exit(0)
+    Energy_each = h_plank * c_photon / i # i nanometer 10**-9
+    Energy_photon += Energy_each # 10**-17
 
 
+    # for g in range(0,350):
+    #     g_wave = g*1 + 350
+    #     if i >= g_wave  and i <= g_wave + 1:
+    #         z[g] += 1
+#
+# wavelength = 350
+# for i in z:
+#
+#     write_me.write(str(wavelength) + '\t')
+#     write_me.write(str(i) + '\n')
+#     wavelength += 1
+# # write_me.write(text)
+# write_me.close()
+#
+# sys.exit(0)
+#
+print(Energy_photon)
 
 
--- scripts/LSC-PM/run_simulation_photovoltaic.py
+++ scripts/LSC-PM/run_simulation_photovoltaic.py
@@ -7,10 +7,10 @@ from pvtrace.lscpm.SolarSimulators import *
 # blue red thickness: 3mm green: 4mm
 # blue not yet, green EY, red Methylene Blue,
 # set episilon to prevent the possibility of matching fate and generate
-file_path = os.path.join('D:/','LSC_PM_simulation_results', 'photovoltaic', 'PV_redLSC_PM', 'simulation_results.txt')
+file_path = os.path.join('D:/','LSC_PM_simulation_results', 'photovoltaic', 'noPhotocatalyst_attached_PhotoVoltaic', 'simulation_results.txt')
 
 # scene = pvtrace.Scene(level=logging.INFO, uuid="Fang_rebuttal2_6")
-scene = pvtrace.Scene(level=logging.INFO, uuid='0312final_try')
+scene = pvtrace.Scene(level=logging.INFO, uuid='noPhotocatalyst_attached_PhotoVoltaic')
 logger = logging.getLogger('pvtrace')
 
 # Create LSC-PM DYE material and host material
@@ -18,8 +18,8 @@ Lm_lr305 = LuminophoreMaterial('Limacryl_lr305', 1)#red
 pmma = Matrix('PMMA')
 
 # !!Change the LSC material while simulating the blank LSC
-reactor = Reactor(reactor_name="10x10_chong_thickness0.3cm", luminophore=Lm_lr305, matrix=pmma, photocatalyst="MB",
-                  photocatalyst_concentration=0.0012, solvent='ACN', exist_photovoltaic_bottom=True, exist_photovoltaic_edge=True)
+reactor = Reactor(reactor_name="47x47_chong_thickness0.8cm", luminophore=Lm_lr305, matrix=pmma, photocatalyst="MB",
+                  photocatalyst_concentration=0, solvent='air', exist_photovoltaic_bottom=True, exist_photovoltaic_edge=True)
 scene.add_objects(reactor.scene_obj)
 # seeeqe = reactor.photovoltaic.external_QE(file_name = 'D:\PvTrace_git\pvtrace-fork\data\photovoltaic\External_quantum_efficiency.txt')
 # print(seeeqe)
@@ -34,13 +34,16 @@ lamp.set_lightsource(irradiated_length=reactor.lsc.size[0], irradiated_width=rea
 # lamp.set_lightsource(irradiated_area=(reactor.lsc.size[0], 0.15035), distance=0.025)
 # lamp.move_lightsource(vector=(0, 0.01735))
 
-trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100, use_visualiser=False,
+trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100000, use_visualiser=False,
                        show_axis=False, show_counter=False, db_split=True, preserve_db_tables=True)
 # set color on Trace.py while visualizing
 
 # Run simulation
-
+tic = time.clock()
+logger.info('Simulation Started (time: '+str(tic)+')')
 trace.start()
+toc = time.clock()
+logger.info('Simulation Ended (time: '+str(toc)+', elapsed: '+str(toc-tic)+' s)')
 
 scene.stats.print_detailed()
 text_head = str(scene.stats.print_excel_header(photovoltaic=True) + "\n")

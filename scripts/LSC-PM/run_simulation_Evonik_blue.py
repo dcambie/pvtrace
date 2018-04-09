4c85892b1d294dae6e5e2980e5f1a89f03a21085
shenchong <shenchong@tju.edu.cn>
2018-04-09 09:56:18 +0200

    absorption data of blue LSC and rubpy3

---
+++ scripts/LSC-PM/run_simulation_Evonik_blue.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from pvtrace.lscpm.Reactor import *
+from pvtrace.lscpm.Dyes import *
+from pvtrace.lscpm.Matrix import *
+from pvtrace.lscpm.SolarSimulators import *
+# blue red thickness: 3mm green: 4mm
+# blue not yet, green EY, red Methylene Blue,
+# set episilon to prevent the possibility of matching fate and generate
+file_path = os.path.join('D:/','LSC_PM_simulation_results', '10x10x0.3_Evonik_blue', 'simulation_results_overwrite.txt')
+
+# scene = pvtrace.Scene(level=logging.INFO, uuid="Fang_rebuttal2_6")
+scene = pvtrace.Scene(level=logging.INFO, uuid='10x10x0.3_Evonik_blue')
+logger = logging.getLogger('pvtrace')
+
+# Create LSC-PM DYE material
+blue_evonik = LuminophoreMaterial('Evonik_Blue', 1)#blue
+# Create polymeric matrix
+pmma = Matrix('PMMA')
+
+# !!Change the LSC material while simulating the blank LSC
+reactor = Reactor(reactor_name="10x10_chong_thickness0.3cm", luminophore=blue_evonik, matrix=pmma, photocatalyst="Rubpy3",
+                  photocatalyst_concentration=0.001, solvent='ACN')
+scene.add_objects(reactor.scene_obj)
+
+# lamp = LightSource(lamp_type='White LEDs')
+# lamp.set_LED_voltage(voltage=8)
+# lamp.set_lightsource(irradiated_area=(0.05, 0.05), distance=0.025)
+lamp = LightSource(lamp_type='Sun')
+
+lamp.set_lightsource(irradiated_length=reactor.lsc.size[0], irradiated_width=reactor.lsc.size[1], distance=0.025)
+# lamp.set_lightsource(irradiated_area=(reactor.lsc.size[0], 0.15035), distance=0.025)
+# lamp.move_lightsource(vector=(0, 0.01735))
+
+trace = pvtrace.Tracer(scene=scene, source=lamp.source, throws=100000, use_visualiser=False,
+                       show_axis=False, show_counter=False, db_split=True, preserve_db_tables=True)
+# set color on Trace.py while visualizing
+
+# Run simulation
+
+trace.start()
+
+scene.stats.print_detailed()
+text = str(scene.stats.print_excel_header() + "\n")
+text += str(scene.stats.print_excel() + "\n")
+write_me = open(file_path, 'a')
+write_me.write(text)
+write_me.close()
+
+sys.exit(0)
+

4c4eae977f5146765cf765aeac865a622b2b289b
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-01-24 21:30:02 +0100

    Force numpy to single thread

--- pvtrace/__init__.py
+++ pvtrace/__init__.py
@@ -2,6 +2,11 @@ __version__ = "2.1.6"
 """
 Optical ray tracing for luminescent materials and spectral converter photovoltaic devices
 """
+import os
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
